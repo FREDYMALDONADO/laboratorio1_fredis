dc6cf6166aa3dece36740783692ed9cc12d1e098
FREDYMALDONADO <mrfredymaldonado@gmail.com>
2022-02-18 21:22:58 -0700

    Laboratorio 1

---
+++ cliente.php
@@ -0,0 +1,116 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Laboratorio 1</title>
+    <nav class="navbar navbar-expand-lg navbar-light bg-danger">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Laboratorio 1</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="index.php">Cliente</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#"></a>
+            </li>
+            <li class="nav-item dropdown">
+             
+                
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="aritmeticos_form.php"></a></li>
+                <li><a class="dropdown-item" href="operadores_form.php"></a></li>
+                <li><a class="dropdown-item" href="producto_form.php"></a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="#"></a></li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true"></a>
+            </li>
+          </ul>
+          <form class="d-flex">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
+      </div>
+    </nav>
+
+  </head>
+  <body>
+  
+<div class="container"><br>
+<div class="row justify-content-center">
+    <div class="col-6 p-5 bg-white shadow-lg rounded">
+      <h3>Nuevo cliente</h3>
+<form method="post" action="recibe_datos.php">
+    <div class="form-group">
+        <label for="codigo">Código</label>
+        <input id="codigo" class="form-control" type="text" name="codigo">
+
+    </div>
+    
+    <div class="form-group">
+        <label for="nombre">Nombres</label>
+        <input id="nombre" class="form-control" type="text" name="nombre">
+        
+    </div>
+
+    <div class="form-group">
+        <label for="apellidos">Apellidos</label>
+        <input id="apellidos" class="form-control" type="text" name="apellidos">
+
+    </div>
+     
+    <div class="form-group">
+        <label for="dui">DUI</label>
+        <input id="dui" class="form-control" type="text" name="dui">
+
+    </div>
+
+    <div class="form-group">
+        <label for="direccion">Dirección</label>
+        <input id="dereccion" class="form-control" type="text" name="direccion">
+    </div>
+
+    <div class="form-group">
+        <label for="telefono">Teléfono</label>
+        <input id="telefono" class="form-control" type="text" name="telefono">
+    </div><br>
+
+
+    <div class="form-group">
+  <button class="btn btn-secondary" type="submit">Enviar</button>
+  </div>
+</form>
+
+    </div>
+</div>
+
+
+ </div>
+
+  
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
